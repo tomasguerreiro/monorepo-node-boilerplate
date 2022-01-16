32830f539028361ad77d2966fd3ece2d584dc3cf
Tomas Guerreiro <tomasguerreiro.dev@gmail.com>
2022-01-16 15:42:50 -0300

    integrando tsconfig no projeto

--- frontend/webpanel/src/App.tsx
+++ frontend/webpanel/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "@web-toolkit-base/uiux";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button />
       </header>
     </div>
   );
