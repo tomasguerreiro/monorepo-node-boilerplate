f3a0187e4612297e12fde180bd61442e5f9981d1
Tomas Araujo <tomas.araujo@next.b.br>
2022-04-19 16:09:29 -0300

    equalizando configuração de typescript

--- backend/service/.eslintrc.js
+++ backend/service/.eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['@monorepo/eslint-config/nestjs'],
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
 };
